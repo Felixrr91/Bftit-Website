9bb4da8ca4b16d4e3b390a650278f9d53deee3c4
codecab <codecab.driver@gmail.com>
2024-03-29 19:45:56 +0100

    fix table names query

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,10 @@ router.get( "/", ( req, res ) => {
     res.send( "Hello world." );
 } );
 
-router.get( "/db", ( req, res ) => {
-    const result = db.query( "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';" );
-    res.send( result );
+router.get( "/db", async ( req, res ) => {
+    const result = await db.query( "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';" );
+
+    res.send( result.rows.map( row => row.table_name ) );
 } );
 
 export default router;
